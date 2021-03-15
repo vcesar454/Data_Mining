741f6c2e96a8674c2fa7c52aaa9d52d460227370
Cesar <cesar.velazquez17@tectijuana.edu.mx>
2021-03-15 13:03:15 -0600

    Se agrego algunas conversiones

--- Practice_3.R
+++ Practice_3.R
@@ -6,19 +6,24 @@ gastos <- c (12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 58
 
 #Calcule las ganancias como las diferencias entre ingresos y gastos
 Beneficio <- ingresos - gastos
+Beneficio
 
 #Calcule el impuesto como el 30% de la ganancia y redondee a 2 decimales
 impuesto <- round(0.30* Beneficio, 2)
+impuesto
 
 #Calcular las ganancias restantes después de deducir los impuestos
 BeneficioDImpuestos <- Beneficio - impuesto
+BeneficioDImpuestos
 
 #Calcular el margen de beneficio como beneficio después de impuestos sobre ingresos
 #Redondee a 2 puntos decimales, luego multiplique por 100 para obtener %
 Margen_Beneficio <- round(BeneficioDImpuestos/ingresos, 2)*100
+Margen_Beneficio
 
 #Calcule el beneficio medio después de impuestos durante los 12 meses
 mean_pat <- mean(BeneficioDImpuestos)
+mean_pat
 
 #Encuentre los meses con beneficios después de impuestos superiores a la media
 #No Funciona!!!!
@@ -38,3 +43,10 @@ for(i in BeneficioDImpuestos){
   }
 }
 MesesMalos
+
+#Convertir todos los calculos a unidades de mil dolares
+ingresos <- round(ingresos/1000, 2)
+ingresos
+gastos <- round(gastos/1000, 2)
+gastos
+
