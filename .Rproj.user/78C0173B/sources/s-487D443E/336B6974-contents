6d71d4749d90f3b5db221dd7a24e11429ca1f071
Cesar <cesar.velazquez17@tectijuana.edu.mx>
2021-03-15 20:22:06 -0600

    Project restructuring

---
+++ .Rproj.user/78C0173B/sources/s-487D443E/336B6974-contents
@@ -0,0 +1,11 @@
+N <- 900000
+Counter <- 0
+
+for(i in rnorm(N)){
+  if(i >=-1 & i<=1){
+    Counter <- Counter + 1
+  }
+}
+result = Counter/N
+#Se aproxima a E(x) = 68.2% cumpliendo la ley de los numeros
+#grandes
--- Practices/Practice_3.R
+++ Practices/Practice_3.R
@@ -1,55 +1,54 @@
 #PRACTICA NUMERO 3
 
-ingresos <- c (14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
-gastos <- c (12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
-
+Revenue <- c (14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
+Expenses <- c (12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
+Revenue
+Expenses
 
 #Calcule las ganancias como las diferencias entre ingresos y gastos
-Beneficio <- ingresos - gastos
-Beneficio
+Profit <- Revenue - Expenses
+Profit
 
 #Calcule el impuesto como el 30% de la ganancia y redondee a 2 decimales
-impuesto <- round(0.30* Beneficio, 2)
-impuesto
+tax <- round(0.30* Profit, 2)
+tax
 
 #Calcular las ganancias restantes después de deducir los impuestos
-BeneficioDImpuestos <- Beneficio - impuesto
-BeneficioDImpuestos
+Profit_After_tax <- Profit - tax
+Profit_After_tax
 
 #Calcular el margen de beneficio como beneficio después de impuestos sobre ingresos
 #Redondee a 2 puntos decimales, luego multiplique por 100 para obtener %
-Margen_Beneficio <- round(BeneficioDImpuestos/ingresos, 2)*100
-Margen_Beneficio
+Profit_Margin <- round(Profit_After_tax/Revenue, 2)*100
+Profit_Margin
 
 #Calcule el beneficio medio después de impuestos durante los 12 meses
-mean_pat <- mean(BeneficioDImpuestos)
+mean_pat <- mean(Profit_After_tax)
 mean_pat
 
 #Encuentre los meses con beneficios después de impuestos superiores a la media
-MesesBuenos <- BeneficioDImpuestos > mean_pat
+GoodMonths <- Profit_After_tax > mean_pat
 #Los mese buenos fueron Enero, Junio, Julio, Agosto, Septiembre y Diciembre
-MesesBuenos
-
+GoodMonths
 
 # ¡Los meses malos son lo opuesto a los buenos meses!
-MesesMalos <- BeneficioDImpuestos< mean_pat
+BadMonths <- Profit_After_tax< mean_pat
 #Los meses malso fueron  Febrero, Marzo, Abril, Mayo, Octubre y Noviembre 
-MesesMalos
+BadMonths
 
 #El mejor mes es cuando el beneficio después de impuestos era igual al máximo
-MejorMes<-BeneficioDImpuestos == max(BeneficioDImpuestos)
+BestMonth <- Profit_After_tax == max(Profit_After_tax)
 #El mejor mes Fue diciembre
-MejorMes
+BestMonth
 
 # El peor mes es cuando la ganancia después de impuestos era igual al mínimo
-PeorMes<- BeneficioDImpuestos == min(BeneficioDImpuestos)
+WorstMonth <- Profit_After_tax == min(Profit_After_tax)
+
 #El peor mes fue Marzo
-PeorMes
+WorstMonth
 
 #Convertir todos los calculos a unidades de mil dolares
-ingresos <- round(ingresos/1000, 2)
-ingresos
-gastos <- round(gastos/1000, 2)
-gastos
-#Corrigir lo del puchh
-
+Revenue <- round(Revenue/1000, 2)
+Revenue
+Expenses <- round(Expenses/1000, 2)
+Expenses
