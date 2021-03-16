6906b2c8636f6bacc396af9a61ff77cea76a15a3
DanielCamacho200 <juan.camacho17@tectijuana.edu.mx>
2021-03-15 17:11:16 -0700

    Trate de corregir lo que no funsionaba  pro el momento creo que los datos son correctos.

--- Practice_3.R
+++ Practice_3.R
@@ -26,27 +26,30 @@ mean_pat <- mean(BeneficioDImpuestos)
 mean_pat
 
 #Encuentre los meses con beneficios después de impuestos superiores a la media
-#No Funciona!!!!
-for(i in BeneficioDImpuestos){
-  if(BeneficioDImpuestos[i] >= mean_pat){
-    MesesBuenos <- MesesBuenos + 1
-  }
-}
+MesesBuenos <- BeneficioDImpuestos > mean_pat
+#Los mese buenos fueron Enero, Junio, Julio, Agosto, Septiembre y Diciembre
 MesesBuenos
 
+
 # ¡Los meses malos son lo opuesto a los buenos meses!
-#No Funciona!!!!
-MesesMalos <- 0
-for(i in BeneficioDImpuestos){
-  if(BeneficioDImpuestos[i] < mean_pat){
-    MesesMalos <- MesesMalos + 1
-  }
-}
+MesesMalos <- BeneficioDImpuestos< mean_pat
+#Los meses malso fueron  Febrero, Marzo, Abril, Mayo, Octubre y Noviembre 
 MesesMalos
+
+#El mejor mes es cuando el beneficio después de impuestos era igual al máximo
+MejorMes<-BeneficioDImpuestos == max(BeneficioDImpuestos)
+#El mejor mes Fue diciembre
+MejorMes
+
+# El peor mes es cuando la ganancia después de impuestos era igual al mínimo
+PeorMes<- BeneficioDImpuestos == min(BeneficioDImpuestos)
+#El peor mes fue Marzo
+PeorMes
 
 #Convertir todos los calculos a unidades de mil dolares
 ingresos <- round(ingresos/1000, 2)
 ingresos
 gastos <- round(gastos/1000, 2)
 gastos
+
 
