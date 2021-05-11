119a190a66b12736e2584ddedf69431bf85b4ce2
DanielCamacho200 <juan.camacho17@tectijuana.edu.mx>
2021-05-10 22:39:03 -0700

    we explore the nature of the data with the corresponding commands.

--- Evaluations/Evaluation.R
+++ Evaluations/Evaluation.R
@@ -3,14 +3,14 @@ setwd('C:\\Users\\vcesa\\Desktop\\Tareas\\8vo Semestre\\Minería de Datos\\sourc
 datos = read.csv('Project-Data.csv')
 
 
-#Exploring Data
-datos
-head(datos)
-tail(datos)
-colnames(datos)
-nrow(datos)
-ncol(datos)
-summary(datos)
+  #Exploring Data
+  datos
+  head(datos)
+  tail(datos)
+  colnames(datos)
+  nrow(datos)
+  ncol(datos)
+  summary(datos)
 
 #Shape of the Data Frame
 cat(nrow(datos), " x ", ncol(datos))
