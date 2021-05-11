4ede7c7b2053e1ecdd6b25e2cfb2164053f4c2c8
DanielCamacho200 <juan.camacho17@tectijuana.edu.mx>
2021-05-10 22:51:52 -0700

    We explore the nature of the data with the corresponding commands.

--- Evaluations/Evaluation.R
+++ Evaluations/Evaluation.R
@@ -1,3 +1,12 @@
 #Getting the dataframe loaded
 setwd('C:\\Users\\vcesa\\Desktop\\Tareas\\8vo Semestre\\Minería de Datos\\source\\Unit_2\\Evaluations')
 datos = read.csv('Project-Data.csv')
+
+#Exploring Data
+datos
+head(datos)
+tail(datos)
+colnames(datos)
+nrow(datos)
+ncol(datos)
+summary(datos)
