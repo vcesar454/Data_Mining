4adf68a7e1446bb6e231fee1ea312e4a98c1daf4
Cesar <cesar.velazquez17@tectijuana.edu.mx>
2021-06-09 22:58:42 -0700

    New line of code

--- Evaluation/Evaluation3.R
+++ Evaluation/Evaluation3.R
@@ -1,4 +1,10 @@
+getwd()
+setwd("C:\\Users\\vcesa\\Desktop\\Tareas\\8vo Semestre\\Minería de Datos\\source\\Unit_3\\Evaluation")
+getwd()
 
 #We look for the data file or we can create a general directory.
 dataset = read.csv(file.choose())
 dataset = read.csv('Social_Network_Ads.csv')
+
+dataset = dataset[3:5]
+dataset
