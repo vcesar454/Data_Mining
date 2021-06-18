2345657bcc3b68e8567520a08e71b01a7f55ddde
Cesar <cesar.velazquez17@tectijuana.edu.mx>
2021-06-17 23:20:08 -0700

    Added the elbow method, it proves that 3 cluster are necesary

--- Evaluation/Evaluative Practice.R
+++ Evaluation/Evaluative Practice.R
@@ -16,3 +16,14 @@ iris.class<- iris[,"Species"]
 iris.new<- iris[,c(1,2,3,4)]
 head(iris.new)
 head(iris.class)
+
+#Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(iris.new, i)$withinss)
+plot(1:10,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
